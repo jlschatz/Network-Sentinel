0d0aaf51775f50e32477f17cd81d03b25211df6e
Jurgen Schatz <jurgen.schatz@gmail.com>
2023-06-24 14:15:54 +0100

    Update storage functionality

---
+++ local_testing_config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export COLLECTION="Network_Sentinel"
+export MONGO_DATABASE="test"
+export MONGO="mongodb://localhost:27017"
+
+source ~/.bashrc
--- storage/service.go
+++ storage/service.go
@@ -2,9 +2,12 @@ package storage
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
 	"os"
 	"sentinel/models"
 )
@@ -15,6 +18,8 @@ type Store interface {
 	Upsert(ip, mac string) error
 	Find(mac string) (models.Entity, error)
 	FindAll() ([]models.Entity, error)
+	ValidateEnvironmentVariables() error
+	SetTestEnvironmentVariables() error
 }
 
 type store struct {
@@ -23,17 +28,26 @@ type store struct {
 }
 
 func NewStore() Store {
-	return &store{}
+	s := &store{}
+	if err := s.SetTestEnvironmentVariables(); err != nil {
+		log.Fatal(err)
+	}
+	if err := s.ValidateEnvironmentVariables(); err != nil {
+		log.Fatal(err)
+	}
+	return s
 }
 
 func (s *store) NewDatabaseConnection() error {
 
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("MONGO")))
 	if err != nil {
+		fmt.Println(fmt.Sprintf("Failed to connect to database: %s", err.Error()))
 		return err
 	}
 
 	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		fmt.Println(err.Error())
 		return err
 	}
 
@@ -44,4 +58,31 @@ func (s *store) NewDatabaseConnection() error {
 
 func (s *store) CloseDatabaseConnection() error {
 	return s.db.Client().Disconnect(context.TODO())
+}
+
+func (s *store) SetTestEnvironmentVariables() error {
+	if err := os.Setenv("MONGO", "mongodb://localhost:27017"); err != nil {
+		return err
+	}
+	if err := os.Setenv("MONGO_DATABASE", "test"); err != nil {
+		return err
+	}
+	if err := os.Setenv("COLLECTION", "Network_Sentinel"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *store) ValidateEnvironmentVariables() error {
+	if os.Getenv("MONGO") == "" {
+		return errors.New("MONGO env variable not set")
+	}
+	if os.Getenv("MONGO_DATABASE") == "" {
+		return errors.New("MONGO_DATABASE env variable not set")
+	}
+	if os.Getenv("COLLECTION") == "" {
+		return errors.New("COLLECTION env variable not set")
+	}
+
+	return nil
 }
